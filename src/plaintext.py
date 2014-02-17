1eb6dcee1d964d82109284b86d98a83d7f851708
Anders Einar Hilden <hildenae@gmail.com>
2014-02-17 18:40:58 +0100

    Make /plaintext use new library SoppParser.py

--- src/plaintext.py
+++ src/plaintext.py
@@ -1,16 +1,16 @@
 import config # config variables
 import webapp2 # serving web content via gae
-import lxml.html # parsing html
-import scraper
+import SoppParser # parser for sopp.no
 
 class MainPage(webapp2.RequestHandler):
 	def get(self):
 		self.response.headers['Content-Type'] = 'text/plain'
-		root = lxml.html.fromstring(scraper.scrape(config.SOPP_URL, "Sopp-Middag-Scraper-hildenae-at-gmail-com"))
-		table = scraper.extractTable(root)
-		ctable = scraper.cleanup(table)
-		d = scraper.tableToDict(ctable);
-		self.response.write(scraper.plainText(d, ';'));
+		hig = SoppParser.SoppParser(config.SOPP_HIG)
+		hig.scrape()
+		hig.extract()
+		hig.verify()
+		hig.parse()
+		self.response.write(hig.plainText());
 
 application = webapp2.WSGIApplication([
     ('/plaintext', MainPage),

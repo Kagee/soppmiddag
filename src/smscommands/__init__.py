e5a3083bce43764d2a610b45fa381861443321e5
Anders Einar Hilden <hildenae@gmail.com>
2014-02-20 22:28:30 +0100

    Cleanup modsms, make into package

--- src/smscommands/__init__.py
+++ src/smscommands/__init__.py
@@ -11,19 +11,11 @@ def getModule(sms):
 
 	# maxlength of ten alafanumeric chars for word
 	if not re.match(r"^[a-z0-9]{1,10}$", word):
-		print "regexp failed"
+		logging.info("Regexp failed for command: %s" % (word))
 		return None
-
 	try:
-		module = importlib.import_module(word, "smsmodules")
+		module = importlib.import_module("." + word, "smscommands")
 		return module
 	except ImportError as e:
-		print "module load failed: %s" % (e)
+		loggin.info("Module load failed: %s" % (e))
 		return None
-
-if __name__ == "__main__":
-	print getModule("foo")
-	print getModule("middag")
-	print getModule("middag hil")
-	print getModule("ymp3 https://www.youtube.com/watch?v=aWuDWUdjWH4")
-	print getModule("asdfghjkloiu")
